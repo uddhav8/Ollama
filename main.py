37a73443a547f03d7a0f462753ce5d0547155628
uddhav8 <uddhav.labde@proton.me>
2024-11-01 18:02:04 +0530

    How to create a prompt template

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+'''
+LANGCHAIN_TRACING_V2="true"
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY="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"
+LANGCHAIN_PROJECT="pr-kindly-address-51"
+'''
+'''
+export LANGCHAIN_TRACING_V2="true"
+export LANGCHAIN_API_KEY="..."
+'''
 
 # Define LLM Provider.
 model = ChatOllama(
@@ -9,12 +21,20 @@ model = ChatOllama(
     # other params ...
 )
 
-# Mesages to the LLM and the User.
-messages = [
-    SystemMessage(content="Translate the following from English into Spanish"),
-    HumanMessage(content="Hello! How are you?"),
-]
+# Define the message template to provide to the llm.
+system_template = "Translate the following into {language}:" # language will be replaced by user input
 
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template), 
+        ("user", "{text}")
+    ] # The messages to send to the LLM.
+)
+
+result = prompt_template.invoke({"language": "Spanish", "text": "Hello! How are you?"})
+print(result)
+
+'''
 # Define Output Parser
 parser = StrOutputParser()  # This parses the output of the LLM into a more human readable.
 
@@ -22,4 +42,4 @@ parser = StrOutputParser()  # This parses the output of the LLM into a more huma
 chain = model | parser
 
 # print output in terminal
-print(chain.invoke(messages))
+print(chain.invoke(messages))'''
