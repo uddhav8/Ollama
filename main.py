4c14229f2c80c122d19c3d06c5a7e9fc1face07a
uddhav8 <uddhav.labde@proton.me>
2024-11-01 17:37:32 +0530

    Basic works

--- main.py
+++ main.py
@@ -2,30 +2,17 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 
-llm = ChatOllama(
-    model = "llama3.2:latest",
-    base_url= "http://192.168.0.64:11434/",
+# Define LLM Provider.
+model = ChatOllama(
+    model = "llama3.2:latest", # The model name and version to use for inference.
+    base_url= "http://192.168.0.64:11434/", # The URL of the LLM.
     # other params ...
 )
 
+# Mesages to the LLM and the User.
 messages = [
-    ("system", "You are a helpful translator. Translate the user sentence to French."),
-    ("human", "I love programming."),
+    SystemMessage(content="Translate the following from English into Spanish"),
+    HumanMessage(content="Hello! How are you?"),
 ]
 
-'''messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
-]'''
-
-print(llm.invoke(messages))
-
-
-'''
-parser = StrOutputParser()
-
-result = llm.invoke(messages)
-
-chain = llm | parser
-
-chain.invoke(result)'''
+print(model.invoke(messages))
