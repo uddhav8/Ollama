2bb799c5116fa54599aa99f14d5da752d08367b6
uddhav8 <uddhav.labde@proton.me>
2024-11-02 09:10:51 +0530

    same same but different

--- SimpleChatBot/main.py
+++ SimpleChatBot/main.py
@@ -1,5 +1,7 @@
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage 
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 # Define LLM Provider.
 model = ChatOllama(
@@ -8,10 +10,32 @@ model = ChatOllama(
     # other params ...
 )
 
-model.invoke(
-    [
-        HumanMessage(content="Hi! I'm Bob"),
-        AIMessage(content="Hello Bob! How can I assist you today?"),
-        HumanMessage(content="What's my name?"),
-    ]
-)
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "abc123"}}
+
+query = "Hi! I'm Bob."
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()  # output contains all messages in state
+
+query = "What's my name?"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
