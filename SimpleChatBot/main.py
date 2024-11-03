ff24270f53a423a20c4494f61d8e67c5f9162782
uddhav8 <uddhav.labde@proton.me>
2024-11-03 16:56:50 +0530

    Adding a language variable to the prompt

--- SimpleChatBot/main.py
+++ SimpleChatBot/main.py
@@ -1,8 +1,11 @@
 from langchain_ollama import ChatOllama
-from langchain_core.messages import HumanMessage, AIMessage 
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.graph.message import add_messages
+from typing import Sequence
+from typing_extensions import Annotated, TypedDict
 
 # Define LLM Provider.
 model = ChatOllama(
@@ -15,20 +18,24 @@ prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You talk like a pirate. Answer all questions to the best of your ability.",
+            "You area helpful assistant. Answer all questions to the best of your ability in {language}.",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
+
 # Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
+workflow = StateGraph(state_schema=State)
 
 # Define the function that calls the model
-def call_model(state: MessagesState):
+def call_model(state: State):
     chain = prompt | model
     response = chain.invoke(state)
-    return {"messages": response}
+    return {"messages": [response]}
 
 # Define the (single) node in the graph
 workflow.add_edge(START, "model")
@@ -37,7 +44,7 @@ workflow.add_node("model", call_model)
 # Add memory
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
-
+'''
 config = {"configurable": {"thread_id": "abc123"}}
 
 query = "Hi! I'm Bob."
@@ -52,6 +59,7 @@ input_messages = [HumanMessage(query)]
 output = app.invoke({"messages": input_messages}, config)
 output["messages"][-1].pretty_print()
 '''
+'''
 config = {"configurable": {"thread_id": "abc234"}}
 
 input_messages = [HumanMessage(query)]
@@ -64,3 +72,23 @@ input_messages = [HumanMessage(query)]
 output = app.invoke({"messages": input_messages}, config)
 output["messages"][-1].pretty_print()
 '''
+config = {"configurable": {"thread_id": "abc456"}}
+query = "Hi! I'm Bob."
+language = "Spanish"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke(
+    {"messages": input_messages, "language": language},
+    config,
+)
+output["messages"][-1].pretty_print()
+
+
+query = "What is my name?"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke(
+    {"messages": input_messages},
+    config,
+)
+output["messages"][-1].pretty_print()
